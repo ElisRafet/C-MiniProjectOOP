83eea691462eb94af24c5637146e917dcbfd46ab
Elis Rafey <elisrafet2@gmail.com>
2025-06-14 18:09:37 +0300

    Project added to the repository.

---
+++ prototip/prototip.cpp
@@ -0,0 +1,136 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class taxPayer
+{
+public:
+	taxPayer() {};
+	~taxPayer() {};
+	virtual void print_Info() = 0; 
+	virtual double calc_Tax() = 0; 
+
+protected:
+	string ID;
+};
+
+class taxDot : public taxPayer
+{
+public:
+	taxDot(string ime, double suma) { ID = ime; dohod = suma; };
+	~taxDot() {};
+	virtual void print_Info() {
+		cout << "DOD: Ime:" << ID << ", Dohod:" << dohod << ", Tax:" << calc_Tax() << endl;
+	};
+	virtual double calc_Tax() { return dohod * 0.1; };
+
+private:
+	double dohod;
+};
+
+class taxEstate : public taxPayer
+{
+public:
+	taxEstate(string adres, double ocenka) { ID = adres; dan_ocenka = ocenka; };
+	~taxEstate() {}
+	virtual void print_Info() {
+		cout << "ESTATE: Adres:" << ID << ", Ocenka:" << dan_ocenka << ", Tax:" << calc_Tax() << endl;
+	};
+	virtual double calc_Tax() { return dan_ocenka * 0.001; };
+
+private:
+	double dan_ocenka;
+
+};
+
+class taxCar :public taxPayer
+{
+public:
+	taxCar(string reg, int po, int ag) { ID = reg; power = po; age = ag; };
+	~taxCar() {};
+	virtual void print_Info() {
+		cout << "MPS: Reg:" << ID << ", Moshtnost:" << power << ", Vazrast:" << age << ", Tax:" << calc_Tax() << endl;
+	};
+	virtual double calc_Tax() {
+		double baza = 300;
+		if (power > 100) baza *= 1.3;
+		baza = baza - (age * 0.05) * baza;
+		return baza;
+	};
+
+private:
+	int power, age;
+};
+
+class taxQueue
+{
+public:
+	taxQueue() {};
+	~taxQueue() {};
+	void Add(taxPayer* obj) { opashka.push_back(obj); };
+	void List() {
+		if (opashka.empty()) {
+			cout << "Nqma nikoi\n";
+		}
+		else {
+			cout << "Na opashkata sa:\n";
+			for (int x = 0; x < opashka.size(); x++)
+				opashka[x]->print_Info();
+		}
+	};
+	double Proccess() {
+		if (opashka.empty()) {
+			cout << "Nqma nikoi\n";
+			return 0;
+		}
+		else {
+			double tax = opashka[0]->calc_Tax();
+			opashka.erase(opashka.begin());
+			return tax;
+		}
+	};
+
+private:
+	vector <taxPayer*> opashka;
+};
+
+int main()
+{
+	taxQueue q;
+
+	while (true) {
+		cout << "\n\nMENU\n===========================\n";
+		cout << "1. Dobavqne na DOD\n2. Dobavqne na ESTATE\n3. Dobavqne na CAR\n";
+		cout << "4. Izvejdane\n5. Obrabotka\n0. Izhod\n";
+
+		cout << endl;
+		cout << "Vuvedi: ";
+
+		string izb;
+		getline(cin, izb);
+
+		if (izb == "0") break;
+		if (izb == "4") q.List();
+		if (izb == "5") cout << "Plateni:" << q.Proccess() << endl;
+		if (izb == "1") {
+			string ime; double suma;
+			cout << "Ime: "; getline(cin, ime);
+			cout << "Dohod: "; cin >> suma;
+			q.Add(new taxEstate(ime, suma));
+		};
+		if (izb == "2") {
+			string adr; double suma;
+			cout << "Adres:"; getline(cin, adr);
+			cout << "Ocenka:"; cin >> suma;
+			q.Add(new taxEstate(adr, suma));
+		};
+		if (izb == "3") {
+			string reg; int p, a;
+			cout << "Reg:"; getline(cin, reg);
+			cout << "Moshtnost:"; cin >> p;
+			cout << "Vuzrast:"; cin >> a;
+			q.Add(new taxCar(reg, p, a));
+		}
+	}
+}
---
+++ кркр/кркр.cpp
@@ -0,0 +1,187 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Mebeli
+{
+public:
+	Mebeli() {};
+	~Mebeli() {};
+	virtual void print_info() = 0;
+	virtual int calc_cena() = 0;
+
+protected:
+	string name;
+	int price; 
+};
+
+class Masa :public Mebeli
+{
+public:
+	Masa(string ime, string razmer, string mat, int cena) {                          //1.malka 
+		name = ime; size = razmer; material = mat; price = cena;                    //2.sredna 
+	};                                                                             //3.golqma 
+	~Masa() {};
+	
+	virtual int calc_cena() {                                                           //1.styklena +80lv
+		if (size == "1") {                                                             //2.dyrwena +50lv
+			if (material == "1")                                                      //3.jelqzna i dyrwena +70lv
+				return price + 80;
+			else if (material == "2")
+				return price + 50;
+			else return price + 70;
+		}
+		else if (size == "2"){
+			if (material == "1")
+				return price *1.2 + 80;
+			else if (material == "2") 
+				return price * 1.2 + 50;
+			else return price * 1.2 + 70; 
+		}
+		else {
+			if (material == "1")
+				return price * 1.5 + 80;
+			else if (material == "2")
+				return price * 1.5 + 50;
+			else return price * 1.5 + 70;
+		}
+	}
+	virtual void print_info() {
+		cout << "Masa - " << name << " Razmer - " << size << ", Material - " << material << ", Cena - " << calc_cena() << "lv." << endl;
+	}
+
+private:
+	string size; string material;
+};
+
+class Divan :public Mebeli
+{
+public:
+	Divan(string ime, string razmer, string razteg, int cena) {
+		name = ime; size = razmer; raztegatelen = razteg; price = cena;
+	};
+	~Divan() {};
+	virtual int calc_cena() {
+		if (size == "1") {
+			if (raztegatelen == "1") return price + 500;
+			else return price;
+		}
+		else if (size == "2") {
+			if (raztegatelen == "1") return price + 500;
+			else return price;
+		}
+		else {
+			if (raztegatelen == "1") return price + 500;
+			else return price;
+		}
+	}
+	virtual void print_info(){
+		cout << "Diwan - " << name << ", Razmer - " << size << ", Raztegatelen - " << raztegatelen << ", Cena - " << calc_cena()  << "lv." << endl;
+	}
+
+private:
+	string size;
+	string raztegatelen;
+};
+
+class Stol :public Mebeli
+{
+public:
+	Stol(string ime, string vid, int cena) {
+		name = ime; type = vid; price = cena;
+	};
+	~Stol() {};
+	virtual int calc_cena() {
+		if (type == "1") return price;
+		else if (type == "2") return price + 50;
+		else return price + 100;
+	}
+	virtual void print_info() {
+		cout << "Stol - " << name << ", Wid stol - " << type << ", Cena - " << calc_cena() << "lv." << endl;
+	}
+
+private:
+	string type;
+};
+class Proizvodstwo
+{
+public:
+	Proizvodstwo() {};
+	~Proizvodstwo() {};
+	void Add(Mebeli* obj) { 
+		spisyk.push_back(obj); 
+		cout << "Poruchkata e prieta." << endl << endl;
+	};
+	void List() {
+		if (spisyk.empty()) {
+			cout << "Nqma porychki za mebeli.\n";
+		}
+		else {
+			cout << "Prorychkite za mebeli sa: \n";
+			for (int x = 0; x < spisyk.size(); x++)
+				spisyk[x]->print_info();
+		}
+	};
+	int Obrabotka() {
+		if (spisyk.empty()) {
+			cout << "Nqma porychki za mebeli.\n";
+			return 0;
+		}
+		else {
+			cout << "Wywedete nomer na porychka: "; cin >> e;
+			if ((e > 0) && (e - 1< spisyk.size())) {
+				int price = spisyk[e-1]->calc_cena();
+				spisyk.erase(spisyk.begin() + (e -1));
+				return price;
+			}
+			else {
+				cout << "Greshen nomer na porychka!!!!!" << endl << endl;
+				return 0;
+			}
+		}
+	};
+
+private:
+	vector <Mebeli*> spisyk;
+	int e;
+};
+,
+int main()
+{
+	Proizvodstwo m;
+	string opciq;
+
+	while (true) {
+		cout << "---------------------Menu---------------------" << endl;
+		cout << "1.Porychka na masa\n2.Porychka na divan\n3.Poruchka na stol\n4.Izvejdane na spisyka s poruchki\n5.Obrabotka na porychka\n0.Izhod" << endl << endl;
+		cout << "Izberete opciq: "; getline(cin, opciq);
+
+		if (opciq == "0") break;
+		if (opciq == "1") {
+			string name, size, material; int price;
+			cout << "Izberete model na masa: Victoria, Sophia, Mara.\nModel: "; getline(cin, name);
+			cout << "Izberete edin ot slednite razmeri:\n1.Malka\n2.Sredna\n3.Golqma\nRazmer: "; getline(cin, size);
+			cout << "Izberete edin slednite materiali: \n1.Styklo\n2.Dyrwo\n3.Jelqzo\nMaterial: "; getline(cin, material);
+			cout << "Kakwa da e nachalnata cena? "; cin >> price;
+			m.Add(new Masa(name, size, material, price));
+		}
+		if (opciq == "2") {
+			string name, size, raztegatelen; int price;
+			cout << "Izberete model na diwan: Victoria, Sophia, Mara.\nModel: "; getline(cin, name);
+			cout << "Izberete edin ot slednite razmeri:\n1.Malyk\n2.Sreden\n3.Golqm\nRazmer: "; getline(cin, size);
+			cout << "Da byde li raztegatelen?   1.Da   2.Ne\nOtgovor: "; getline(cin, raztegatelen);
+			cout << "Kakwa da e nachalnata cena? "; cin >> price;
+			m.Add(new Divan(name, size, raztegatelen, price));
+		}
+		if (opciq == "3") {
+			string name, vid; int price;
+			cout << "Izberete model na stol: Victoria, Sophia, Mara.\nModel: "; getline(cin, name);
+			cout << "Izberete edna ot slednite opcii: \n1.Dyrwen\n2.Jelezen\n3.Wyrtqsht se\nOpciq: "; getline(cin, vid);
+			cout << "Kakwa da e nachalnata cena? "; cin >> price;
+			m.Add(new Stol(name, vid, price));
+		}
+		if (opciq == "4") m.List();
+		if (opciq == "5") cout << "Obrabotena porucka - " << m.Obrabotka() << endl;
+	}
+}
